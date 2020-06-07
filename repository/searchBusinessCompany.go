92a65176db3640f82aab634c6c41cd378750e7ad
Akezhan <21172@iitu.kz>
2020-06-07 23:30:50 +0600

    fixes

--- repository/searchBusinessCompany.go
+++ repository/searchBusinessCompany.go
@@ -1,1 +1,53 @@
 package repository
+
+import (
+	pb "github.com/AkezhanOb1/business-company/api"
+	"github.com/AkezhanOb1/business-company/config"
+	"github.com/jackc/pgx/v4"
+	"context"
+
+)
+
+
+//SearchBusinessCompanyRepository is a repository that responsible to all the requests to DB
+//about business categories
+func SearchBusinessCompanyRepository(ctx context.Context, companyName string) (*pb.SearchBusinessCompanyResponse, error) {
+	conn, err := pgx.Connect(ctx, config.PostgresConnection)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close(ctx)
+
+
+	companyName = companyName + "%"
+	sqlQuery := `SELECT id, name, category_id, address 
+					FROM business_company
+				 WHERE LOWER(name) LIKE LOWER($1);`
+
+	rows, err := conn.Query(ctx, sqlQuery, companyName)
+	if err != nil {
+		return nil, err
+	}
+
+	var companies pb.SearchBusinessCompanyResponse
+
+	for rows.Next() {
+		var company pb.BusinessCompany
+
+		err = rows.Scan(
+			&company.BusinessCompanyID,
+			&company.BusinessCompanyName,
+			&company.BusinessCompanyCategoryID,
+			&company.BusinessCompanyAddress,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		companies.BusinessCompanies = append(companies.BusinessCompanies, &company)
+	}
+
+	return &companies, nil
+}
--- service/searchBusinessCompany.go
+++ service/searchBusinessCompany.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"context"
+	pb "github.com/AkezhanOb1/business-company/api"
+	db "github.com/AkezhanOb1/business-company/repository"
+)
+
+//GetBusinessCompanies is
+func (*Server) SearchBusinessCompany(ctx context.Context, request *pb.SearchBusinessCompanyRequest) (*pb.SearchBusinessCompanyResponse, error) {
+	businessCompanies, err := db.SearchBusinessCompanyRepository(ctx, request.GetBusinessCompanyName())
+	if err != nil {
+		return nil, err
+	}
+
+	for i, company := range businessCompanies.BusinessCompanies {
+		businessCompanyImages, err := db.GetBusinessCompanyImagesRepository(context.Background(), company.GetBusinessCompanyID())
+		if err != nil {
+			return nil, err
+		}
+
+		businessCompanies.BusinessCompanies[i].BusinessCompanyImages = businessCompanyImages
+	}
+
+	return businessCompanies, nil
+}
